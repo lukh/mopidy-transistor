06e1b971d859ef37fb8c5b716cadcf6b476f7f8b
Vivien Henry <vivien.henry@outlook.fr>
2019-09-07 15:21:59 +0200

    wifi script

--- mopidy_redbox/wifi.py
+++ mopidy_redbox/wifi.py
@@ -1,3 +1,4 @@
+
 import os
 
 def scan_networks(interface="wlan0"):
@@ -11,10 +12,48 @@ def scan_networks(interface="wlan0"):
 
     return [i for i in set(ssid_list) if i != ""]
 
+
+# pi@raspberrypi:~$ sudo systemctl start dhcpcd
+# pi@raspberrypi:~$ sudo systemctl start dnsmasq
+# pi@raspberrypi:~$ sudo systemctl start hostapd
+#interface wlan0
+#    static ip_address=192.168.4.1/24
+#    nohook wpa_supplicant
+
 def turn_on_ap():
-    # need to turn on static ip in dhcpcd.conf...
+    os.popen("sudo systemctl stop dhcpcd")
+
+    os.popen("sudo cp /etc/dhcpcd.conf.source /etc/dhcpcd.conf")
+    os.popen('sudo echo "interface wlan0" >> /etc/dhcpcd.conf')
+    os.popen('sudo echo "    static ip_address=192.168.4.1/24" >> /etc/dhcpcd.conf')
+    os.popen('sudo echo "    nohook wpa_supplicant" >> /etc/dhcpcd.conf')
+    os.popen("sudo systemctl start dhcpcd")
     
     os.popen("sudo systemctl start dnsmasq")
-    os.popen("sudo systemctl start hostap")
-    
-    os.popen("sudo dhcpcd --nohook wpa_supplicant -S ip_address=192.168.4.1/24 wlan0")
+    os.popen("sudo systemctl start hostapd")
+
+
+
+
+def turn_off_ap():
+    os.popen("sudo systemctl stop hostapd")
+    os.popen("sudo systemctl stop dnsmasq")
+
+    os.popen("ip addr flush dev wlan0")
+    os.popen("ip link set dev wlan0 up")
+
+
+    # dhcpcd  -n "$wifidev"
+    os.popen("sudo systemctl stop dhcpcd")
+    os.popen("sudo cp /etc/dhcpcd.conf.source /etc/dhcpcd.conf")
+    # edit file dhcpcd remove wlan0 stuff
+    os.popen("sudo systemctl start dhcpcd")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2):
+        if sys.argv[1] == "on":
+            turn_on_ap()
+
+        else:
+            turn_off_ap()
