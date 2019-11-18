d17ad93f8dd79b72d57a0c7844b500c522e63145
Vivien Henry <vivien.henry@outlook.fr>
2019-11-18 15:57:55 +0100

    using higher version of mopidy

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "setuptools",
-        "Mopidy >= 3.0",
+        "Mopidy >= 2.0",
         "Pykka >= 1.1",
         "pyserial",
         "transitions",
