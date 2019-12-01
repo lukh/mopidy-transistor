a0267673d2a845d6c4dd60d979e3dc2ff66ded1b
Vivien Henry <vivien.henry@outlook.fr>
2019-12-01 18:58:52 +0100

    start test

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -11,15 +11,27 @@ def test_get_default_config():
     assert "[transistor]" in config
     assert "enabled = true" in config
 
+    assert "serial_port = /dev/ttyUSB0" in config
+    assert "serial_baudrate = 115200" in config
+    assert "podcasts_timeout = 5" in config
+    assert "noise_folder = " in config
+    assert "config_file = " in config
+    assert "user = " in config
+    assert "passwd =" in config
+
 
 def test_get_config_schema():
     ext = Extension()
 
     schema = ext.get_config_schema()
 
-    # TODO Test the content of your config schema
-    # assert 'username' in schema
-    # assert 'password' in schema
+    assert 'serial_port' in schema
+    assert 'serial_baudrate' in schema
+    assert 'podcasts_timeout' in schema
+    assert 'noise_folder' in schema
+    assert 'config_file' in schema
+    assert 'user' in schema
+    assert 'passwd' in schema
 
 
 # TODO Write more tests
